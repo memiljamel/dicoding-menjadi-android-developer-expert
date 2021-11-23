f1085e27c9e34ee8a622a068aa221c1365cc4a41
Emil Jamel Mahmuda <memiljamel@gmail.com>
2021-11-23 12:30:07 +0700

    Memperbaiki saran yang diberikan oleh Inspect Code Analyze

--- favorite/src/main/java/com/example/tmdb/favorite/FavoriteViewModel.kt
+++ favorite/src/main/java/com/example/tmdb/favorite/FavoriteViewModel.kt
@@ -3,7 +3,6 @@ package com.example.tmdb.favorite
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.example.tmdb.core.domain.usecase.MovieUseCase
-import javax.inject.Inject
 
 class FavoriteViewModel(movieUseCase: MovieUseCase) : ViewModel() {
     val favorite = movieUseCase.getFavoriteMovie().asLiveData()
